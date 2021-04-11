42c1911eaada91d45be72e5438e2b83ff435a1f9
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-11 08:52:45 +0200

    Use Form component in home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import Layout from '../components/Layout';
+import { FlexCenter } from '../components/Flex';
+import Form from '../components/Form';
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Layout>
+      <FlexCenter className="h-full bg-gray-100">
+        <Form />
+      </FlexCenter>
+    </Layout>
+  );
 }
