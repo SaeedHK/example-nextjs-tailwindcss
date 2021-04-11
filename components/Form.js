76c39fcf9e1c18ce84fd8e131045cf7e3a15ea20
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-11 09:25:35 +0200

    Use ToolTip prop as JSX

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { FlexCenter, FlexCol, FlexRow } from './Flex';
 
-const Field = ({ label, toolTip }) => {
+const Field = ({ label, ToolTip }) => {
   const [tooltipIsActive, setTooltipIsActive] = useState(false);
   return (
     <div className="relative w-full">
@@ -18,14 +18,14 @@ const Field = ({ label, toolTip }) => {
         <input className="w-full px-2 text-black bg-gray-200 border-0 rounded-r-lg focus:outline-none" />
       </FlexRow>
 
-      {toolTip && (
+      {ToolTip && (
         <div
           className={
             'p-3 text-sm text-gray-500 mt-1 w-full bg-gray-200 rounded-b-lg border-t-2 border-green-400 transition duration-500 delay-1000 absolute ' +
             (tooltipIsActive ? 'opacity-100 z-20' : 'opacity-0 invisible')
           }
         >
-          {toolTip}
+          <ToolTip />
         </div>
       )}
     </div>
@@ -33,31 +33,47 @@ const Field = ({ label, toolTip }) => {
 };
 
 const Form = () => {
-  const toolTip = ` Vestibulum rhoncus congue lacus. Vivamus euismod,
-		felis quis commodo viverra, dolor elit dictum ante, et mollis
-	eros augue at est. Class aptent taciti sociosqu ad litora torquent per
-	    conubia nostra, per inceptos himenaeos. Nulla lectus sem, tristique
-		sed, semper in, hendrerit non, sem. Vivamus dignissim massa in
-		ipsum. Morbi fringilla ullamcorper ligula. Nunc turpis. Mauris
-		vitae sapien. Nunc luctus bibendum velit.
+  const ToolTip1 = () => (
+    <div>
+      Eros. Praesent dictum. Maecenas pharetra odio ut dui. Pellentesque ut orci. Sed lobortis,
+      velit at laoreet suscipit, quam est sagittis nibh, id varius ipsum quam ac metus. Phasellus
+      est nibh, bibendum non, dictum sed, vehicula in, sem.
+    </div>
+  );
 
-Morbi faucibus volutpat sapien. Nam ac mauris at justo adipiscing facilisis.
-		Nunc et velit. Donec auctor, nulla id laoreet volutpat, pede
-	erat feugiat ante, auctor facilisis dui augue non turpis. Suspendisse
-	mattis metus et justo. Aliquam erat volutpat. Suspendisse potenti. Nam.
-		`;
+  const ToolTip2 = () => (
+    <div>
+      Integer lacinia. Praesent blandit elementum sapien. Praesent enim mauris, suscipit a, auctor
+      et, lacinia vitae, nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada
+      fames ac turpis egestas. Praesent lacus diam, auctor quis, venenatis in, hendrerit at.
+    </div>
+  );
+
+  const ToolTip3 = () => (
+    <FlexRow className="space-x-3">
+      <img src="/user.svg" className="w-1/3" />
+      <FlexCenter className="text-xs">
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Praesent lacus diam, auctor quis, venenatis in, hendrerit at. Eros. Praesent
+        dictum. Maecenas pharetra odio ut dui. Pellentesque ut orci. Sed lobortis, velit at laoreet
+        suscipit, quam est sagittis nibh, id varius ipsum quam ac metus. Phasellus est nibh,
+        bibendum non, dictum sed, vehicula in, sem.
+      </FlexCenter>
+    </FlexRow>
+  );
+
   return (
     <form className="w-2/5">
       <FlexCol className="space-y-5">
         <FlexRow className="space-x-5">
-          <Field label="Name" toolTip={toolTip} />
-          <Field label="Family" toolTip={toolTip} />
+          <Field label="Name" ToolTip={ToolTip1} />
+          <Field label="Family" ToolTip={ToolTip2} />
         </FlexRow>
         <FlexRow className="space-x-5">
-          <Field label="Email" toolTip={toolTip} />
-          <Field label="Tel" toolTip={toolTip} />
+          <Field label="Email" ToolTip={ToolTip2} />
+          <Field label="Tel" ToolTip={ToolTip1} />
         </FlexRow>
-        <Field label="Address" toolTip={toolTip} />
+        <Field label="Address" ToolTip={ToolTip3} />
       </FlexCol>
     </form>
   );
