6b0c145b9d171b730213672b37e0a6b021d65bc3
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-11 08:24:50 +0200

    Add new component for Layout and use Noto for font

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import { FlexCol } from './Flex';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Unkle Excercise</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+          integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body className="text-base h-screen">{children}</body>
+    </>
+  );
+};
+
+export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      body: ['Noto Sans JP', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
