1b1af7fb9ffe2efb8a07e0e380c0ded97d83dfab
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2021-04-11 08:49:11 +0200

    Add event listner to flex components: onMouseEnter, onMouseLeave, onClick

--- components/Flex.js
+++ components/Flex.js
@@ -1,35 +1,44 @@
-const FlexCenter = ({ children, className }) => {
+const FlexCenter = ({ children, className, onMouseEnter, onMouseLeave, onClick }) => {
   return (
     <div
       className={`flex flex-row items-center justify-center
 	  ${className}
 	  `}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
     >
       {children}
     </div>
   );
 };
 
-const FlexCol = ({ children, className, center, between }) => {
+const FlexCol = ({ children, className, center, between, onMouseEnter, onMouseLeave, onClick }) => {
   return (
     <div
       className={`flex flex-col
       ${center ? 'justify-center' : ''}
       ${between ? 'justify-between' : ''}
       ${className}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
     >
       {children}
     </div>
   );
 };
 
-const FlexRow = ({ children, className, center, between }) => {
+const FlexRow = ({ children, className, center, between, onMouseEnter, onMouseLeave, onClick }) => {
   return (
     <div
       className={`flex flex-row
       ${center ? 'justify-center' : ''}
       ${between ? 'justify-between' : ''}
       ${className}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
     >
       {children}
     </div>
